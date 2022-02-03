a384b49c1df7c4b696afae202a87b4d7f200f259
hector <iguanax2001@hotmail.com>
2022-02-03 19:48:30 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-gcc puts Programming is like building a multilingual puzzle /n
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
