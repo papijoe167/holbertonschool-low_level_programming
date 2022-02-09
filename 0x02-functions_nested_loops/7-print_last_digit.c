e108ebb19e80160c98cd8813476492ec7d6996de
hector <iguanax2001@hotmail.com>
2022-02-09 20:22:44 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_last_digit - print last digit number
+* @n: n is an integer
+* Return: integer
+*/
+int print_last_digit(int n)
+{
+int last = n % 10;
+if (n < 0)
+last = last * -1;
+_putchar(last + '0');
+return (last);
+}
