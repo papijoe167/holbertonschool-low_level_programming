6088376faec2c4a00cb48b765dfb7af2c8674b5c
hector <iguanax2001@hotmail.com>
2022-02-09 13:42:19 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       int i = 97;
+
+       while (i < 123)
+       {
+putchar(i);
+i++;
+       }
+
+    return (0);
+}
