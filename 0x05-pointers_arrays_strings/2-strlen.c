171aa8f23c96c60010649072817757008cdf580e
hector <iguanax2001@hotmail.com>
2022-02-14 17:47:46 +0000

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - return length of a string.
+* @s: character
+* Return: length of string
+*/
+int _strlen(char *s)
+{
+int len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+		return (len);
+}
