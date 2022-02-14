f7c800891a9fb52db9efce5796c3d9a4bb6dc030
hector <iguanax2001@hotmail.com>
2022-02-14 19:52:48 +0000

    character of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
-* puts2 - prints character of a string.
-* @s: string
+* puts2 - print alternating chars of string.
+* @str: string
 * Return: void
 */
+
 void puts2(char *str)
 {
 	int i;
+
 i = 0;
-while (str[i] != '/0')
+while (str[i] != '\0')
 {
 if (i % 2 == 0)
 _putchar(str[i]);
