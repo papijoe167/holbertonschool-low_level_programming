99abb1aa050a2ecca6c2b7b58fa9f1b957ab1ef1
hector <iguanax2001@hotmail.com>
2022-02-17 20:01:37 +0000

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,28 @@
 #include "main.h"
 /**
-* leet - encodes a string into 1337
-* @s: character
-* Return: string array
-*/
+ * leet - encodes a string into 1337
+ * @s: string.
+ * Return: to the string
+ */
+
 char *leet(char *s)
 {
-	int i;
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (i = 0; i < 5; i++)
+		{
+	if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
 	}
 
 	return (s);
