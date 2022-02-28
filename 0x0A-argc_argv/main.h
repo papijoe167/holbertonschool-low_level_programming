83cd8411c9a672ec30a197f74611f7627ff11774
hector <iguanax2001@hotmail.com>
2022-02-28 17:32:33 +0000

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,1 @@
 int _putchar(char c);
-int main(int arg __attribute__((unused)), char *argv[]);
-int main(int argc, char *argv[] __attribute__((unused)));
-int main(int argc, char *argv[]);
-int main(__attribute__((unused)) int argc, char *argv[]);
