f70009b1d11b33b97db0a840b1b82dd7d7fc834d
hector <iguanax2001@hotmail.com>
2022-02-28 17:48:25 +0000

    multiplies

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,14 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
 * main - multiplies two numbers
 * @argc: number arguments
 * @argv: array of the argument
-* Return: void
+* Return: (0)
 */
 int main(int argc, char *argv[])
 {
-if (argc != 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
